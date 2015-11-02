c77a0d096e1fc778f4455ff1e55436331301f262
zgulde <zach@Zachs-MacBook-Pro.local>
2015-11-02 15:31:24 -0600

    completed exercise 5.4.3

--- php/test.php
+++ php/test.php
@@ -1,29 +1,14 @@
 <?php
 
-$users = 
-[
-	'person1' => 
-	[
-		'first_name' => 'John',
-		'last_name' => 'Doe'
-	],
-	'person2' => 
-	[
-		'first_name' => 'Jane',
-		'last_name' => 'Doe'
-	],
-	'person3' =>
-	[
-		'first_name' => 'Philip',
-		'last_name' => 'Fry'
-	]
-];
+var_dump('test' == 'test');
 
-$name = &$users['person1']['first_name'];
+var_dump('test' == 'Test');
 
-$users['person1']['first_name'] = 'Joe';
+var_dump(100 > 5);
 
-echo $name;
+var_dump('25' === 25);
+
+var_dump('25' !== 25);
 
 echo PHP_EOL;
 
