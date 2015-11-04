6ab2a5a54a878545beba00e326d374cdb682e887
zgulde <zach@Zachs-MacBook-Pro.local>
2015-11-04 11:03:26 -0600

    fomatting

--- php/if.php
+++ php/if.php
@@ -5,35 +5,37 @@ echo PHP_EOL;
  $a = 5;
  $b = 10;
  $c = '10';
-echo "\$a: \n";
+
+echo "\$a: ";
 var_dump($a);
-echo "\$b: \n";
+echo "\$b: ";
 var_dump($b);
-echo "\$c: \n";
+echo "\$c: ";
 var_dump($c);
 echo PHP_EOL;
 
  if ($a < $b) {
-     echo "$a is less than $b\n";
+     echo "\$a is less than \$b\n";
  } elseif ($b > $a) {
-     echo "$b is greater than $a\n";
+     echo "\$b is greater than \$a\n";
  }
 
  if ($b >= $c) {
-     echo "$b is greater than or equal to $c\n";
- } elseif ($b <= $c) {
-     echo "$b is less than or equal to $c\n";
+     echo "\$b is greater than or equal to \$c\n";
+ } else {
+     echo "\$b is less than \$c\n";
  }
 
  if ($b === $c) {
-     echo "$b is equal in type and value $c\n";
+     echo "\$b is equal in type and value \$c\n";
  } elseif ($b == $c) {
-     echo "$b is equal in type to $c\n";
+     echo "\$b is equal in value to \$c\n";
  }
+
  if ($b !== $c) {
-     echo "$b is not identical to $c\n";
+     echo "\$b is not identical to \$c\n";
  } elseif ($b != $c) {
-     echo "$b is not equal to $c\n";
+     echo "\$b is not equal to \$c\n";
  }
 
 
