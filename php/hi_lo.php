b715934b7f6674ace0b8bc886275f45d549268a3
zgulde <zach@Zachs-MacBook-Pro.local>
2015-11-04 11:25:34 -0600

    refactored the hi lo game to accept a max or a min and max number from the command line

--- php/hi_lo.php
+++ php/hi_lo.php
@@ -1,17 +1,25 @@
 <?php
 echo PHP_EOL;
 $user_guess = 0;
-$the_number = mt_rand(1,100);
+$min = 1;
+$max = 100;
+if( ($argc === 2) && ( (int) $argv[1] > 0) ){
+	$max = (int) $argv[1];
+}
+if($argc === 3 && ( (int) $argv[1] < (int) $argv[2]) ){
+	$min = (int) $argv[1];
+	$max = (int) $argv[2];
+}
+$the_number = mt_rand($min, $max);
 
 fwrite(STDOUT,"~~~~~~~~ High-Low Game ~~~~~~~~\n\n");
 
 while($user_guess !== $the_number){
-	$user_guess = 0;
+	$user_guess = $min - 1;
 
-	while( ($user_guess <= 0) || ($user_guess > 100) ){
-		fwrite(STDOUT,"Enter an integer number between 1 and 100: ");
-		$user_guess = fgets(STDIN);
-		$user_guess = (int) $user_guess;
+	while( ($user_guess < $min) || ($user_guess > $max) ){
+		fwrite(STDOUT,"Guess (between $min and $max) : ");
+		$user_guess = (int) fgets(STDIN);
 	}
 
 	fwrite(STDOUT,"===============================\n");
@@ -27,7 +35,6 @@ while($user_guess !== $the_number){
 	fwrite(STDOUT,"===============================\n");
 
 }
-
 
 echo PHP_EOL;
 ?>
