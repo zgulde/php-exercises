5810beca68459d63bb27dfa106b66bdbf95a7dc7
zgulde <zach@Zachs-MacBook-Pro.local>
2015-11-04 21:35:46 -0600

    realized it was a bad idea to try and write fizzbuzz on one line

--- php/fizzbuzz.php
+++ php/fizzbuzz.php
@@ -4,13 +4,18 @@ echo PHP_EOL;
 for($i = 1; $i <= 100; $i++){
 	$message = '';
 
-	if ( ($i % 3) == 0)	$message .= 'fizz';
-	if ( ($i % 5) == 0)	$message .= 'buzz';
+	if ( ($i % 3) == 0)	{
+		$message .= 'fizz';
+	}
+
+	if ( ($i % 5) == 0)	{
+		$message .= 'buzz';
+	}
 
 	echo ($message != '') ? $message : $i;
-
 	echo PHP_EOL;
 }
+
 
 echo PHP_EOL;
 ?>
