204ce069d934217484f279483b54db832eab3f60
zgulde <zach@Zachs-MacBook-Pro.local>
2015-11-06 11:36:40 -0600

    wrote the arithmetic exercise in js

---
+++ public/arithmetic.js
@@ -0,0 +1,50 @@
+var x = 2;
+var y = 14;
+
+function add(a, b)
+{
+    return a + b;
+}
+
+function subtract(a, b)
+{
+    return add(a, -b);
+}
+
+function multiply(a, b)
+{
+    if (b == 0) {
+        return 0;
+    }
+    if (b == 1) {
+        return a;
+    }
+    return add(a, multiply(a, subtract(b, 1) ) );
+    // a + multiply(a, b - 1)
+}
+
+function divide(a, b)
+{
+    if (b === 1) {
+        return a;
+    }
+    if (a === b) {
+        return 1;
+    }
+    if (a < b) {
+        return 0;
+    }
+    return add(1, divide(subtract(a, b), b) );
+    // 1 + divide(a - b, b)
+}
+
+function power(a, b)
+{
+    if (b == 0) {
+        return 1;
+    }
+    return multiply(a, power(a, subtract(b, 1) ) );
+    // a * power(a, b-1)
+}
+
+console.log(x+" to the power of "+y+" equals " + power(x,y));
--- public/index.php
+++ public/index.php
@@ -5,23 +5,7 @@
 	<title>PHP Exercises</title>
 </head>
 <body>
-<?php
-echo PHP_EOL;
-
-$x = 0;
-$y = 5;
-$z = 10;
-
-echo "2.\n";
-var_dump( $x < $y && $y < $z );
-echo "3.\n";
-var_dump( $x > $y && $y < $z );
-echo "4.\n";
-var_dump( $x > $y || $y < $z );
-echo "5.\n";
-var_dump( $x > $y || !($y < $z) );
-
-echo PHP_EOL;
-?>
+<h1>open the console</h1>
+<script src="/arithmetic.js"></script>
 </body>
 </html>
