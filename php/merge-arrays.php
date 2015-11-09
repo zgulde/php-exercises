aeac5b0496161ef899a7138b2005317d9ace3a84
zgulde <zach@Zachs-MacBook-Pro.local>
2015-11-09 12:15:59 -0600

    rewrote the combine_arrays function per the spec of the exercise

--- php/merge-arrays.php
+++ php/merge-arrays.php
@@ -20,14 +20,39 @@ function get_num_in_common($array1, $array2)
     return $num;
 }
 
+//for the purposes of this exercise, we are assuming the arrays have the same
+//length and if they have elements that are identical they will be at the 
+//same index
 function combine_arrays($array1, $array2)
 {
-    foreach ($array2 as $element) {
-        if ( !(is_value_present($element, $array1)) ){
-            array_push($array1, $element);
+    $array_length = count($array1);
+    $combined_array = [];
+    
+    for ($i=0; $i < $array_length; $i++) { 
+        $element1 = array_shift($array1);
+        $element2 = array_shift($array2);
+
+        if ($element1 === $element2) {
+            array_push($combined_array, $element1);
+        } else {
+            array_push($combined_array, $element1);
+            array_push($combined_array, $element2);
         }
     }
-    return $array1;
+    return $combined_array;
 }
 
-print_r(combine_arrays($names, $compare));
+
+// function combine_arrays($array1, $array2)
+// {
+//     foreach ($array2 as $element) {
+//         if ( !(is_value_present($element, $array1)) ){
+//             array_push($array1, $element);
+//         }
+//     }
+//     return $array1;
+// }
+
+print_r( combine_arrays($names, $compare) );
+
+echo PHP_EOL;
