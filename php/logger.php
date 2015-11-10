65ec6af82480725837e8b69c0dcc669183ec69dd
zgulde <zach@Zachs-MacBook-Pro.local>
2015-11-10 12:37:10 -0600

    finished the logger exercise

---
+++ php/logger.php
@@ -0,0 +1,34 @@
+<?php
+echo PHP_EOL;
+
+date_default_timezone_set('America/Chicago');
+
+function log_message($level, $message)
+{
+    $log_entry = PHP_EOL . date('Y-m-d H:i:s') . ' [' . $level . '] ' . $message;
+
+    $filename = 'logs/log-' . date('Y-m-d') . '.log';
+    $handle = fopen($filename, 'a');
+
+    fwrite($handle, $log_entry);
+    fclose($handle);
+}
+
+function log_error($message)
+{
+    log_message('ERROR', $message);
+}
+
+function log_info($message)
+{
+    log_message('INFO', $message);
+}
+
+echo 'logging...' . PHP_EOL;
+
+log_info('This is an info message');
+log_error('This is an error message');
+
+echo 'done!' . PHP_EOL;
+
+echo PHP_EOL;
