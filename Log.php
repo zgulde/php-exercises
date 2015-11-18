d63568c273a268b7423ca3d9ebe483a94fb8a76c
zgulde <zach@Zachs-MacBook-Pro.local>
2015-11-18 11:28:26 -0600

    moved a file

--- Log.php
+++ Log.php
@@ -5,6 +5,11 @@ class Log
     public $handle;
     public $filename;
 
+    /**
+     * sets the default timezone, creates a file with the given prefix and opens
+     * the file for writing
+     * @param string $prefix prefix for the log file
+     */
     public function __construct($prefix = 'log')
     {
         date_default_timezone_set('America/Chicago');
@@ -12,22 +17,38 @@ class Log
         $this->handle   = fopen($this->filename, 'a');
     }
 
+    /**
+     * writes a message to the log file
+     * @param  string $level   level or tag for the message
+     * @param  string $message the message itself
+     */
     public function logMessage($level, $message)
     {
         $log_entry = PHP_EOL . date('Y-m-d H:i:s') . ' [' . $level . '] ' . $message;
         fwrite($this->handle, $log_entry);
     }
 
+    /**
+     * logs an error message
+     * @param  string $message the message to log
+     */
     public function error($message)
     {
         $this->logMessage('ERROR', $message);
     }
 
+    /**
+     * logs and info message
+     * @param  string $message the message to log
+     */
     public function info($message)
     {
         $this->logMessage('INFO', $message);
     }
 
+    /**
+     * closes the log file
+     */
     public function __destruct()
     {
         fclose($this->handle);
